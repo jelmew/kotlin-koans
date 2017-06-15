8bbc32bf5dd504480f67db27768a727017cccaaf
Jelmer van Amen <Jelmer.van.Amen@crv4all.com>
2017-06-15 15:13:49 +0200

    Checkin exercise 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,12 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        if (year - other.year != 0) return year - other.year
+        if (month - other.month != 0) return month - other.month
+        return dayOfMonth - other.dayOfMonth
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
